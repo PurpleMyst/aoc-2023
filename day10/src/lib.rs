e28705495b7e5121563addbb639169a8876150b4
PurpleMyst <PurpleMyst@users.noreply.github.com>
2023-12-10 16:18:41 +0100

    perf(day10): rayon + array maps

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -1,7 +1,7 @@
-use std::{
-    collections::{HashMap, HashSet},
-    fmt::Display,
-};
+use std::fmt::Display;
+
+use ahash::{HashMap, HashMapExt, HashSet, HashSetExt};
+use rayon::prelude::*;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Pipe {
@@ -57,9 +57,9 @@ pub fn solve() -> (impl Display, impl Display) {
         .collect::<Vec<_>>();
 
     let mut q = vec![(start_pos.unwrap(), 0)];
-    let mut distance_map = HashMap::<_, usize>::new();
+    let mut distance_map = vec![vec![usize::MAX; map[0].len()]; map.len()];
     while let Some((n, d)) = q.pop() {
-        let best = distance_map.entry(n).or_insert(d);
+        let best = &mut distance_map[n.1][n.0];
         if *best < d {
             continue;
         }
@@ -77,22 +77,33 @@ pub fn solve() -> (impl Display, impl Display) {
         q.extend_from_slice(&neighbors.map(|n| (n, d + 1)).as_slice());
     }
 
-    let part1 = distance_map.values().max().copied().unwrap();
+    let part1 = distance_map
+        .iter()
+        .flat_map(|row| row.iter())
+        .filter(|&&d| d != usize::MAX)
+        .max()
+        .copied()
+        .unwrap();
 
     let mut left = usize::MAX;
     let mut right = 0;
     let mut top = usize::MAX;
     let mut bottom = 0;
-    for (x, y) in distance_map.keys() {
-        left = left.min(*x);
-        right = right.max(*x);
-        top = top.min(*y);
-        bottom = bottom.max(*y);
+    for y in 0..map.len() {
+        for x in 0..map[y].len() {
+            if distance_map[y][x] == usize::MAX {
+                continue;
+            }
+            left = left.min(x);
+            right = right.max(x);
+            top = top.min(y);
+            bottom = bottom.max(y);
+        }
     }
 
     let candidates = (left..=right)
         .flat_map(|x| (top..=bottom).map(move |y| (x, y)))
-        .filter(|&(x, y)| !distance_map.contains_key(&(x, y)));
+        .filter(|&(x, y)| distance_map[y][x] == usize::MAX);
 
     let width = right - left + 1;
     let height = bottom - top + 1;
@@ -101,150 +112,70 @@ pub fn solve() -> (impl Display, impl Display) {
     // means every pipe + its neighbors
     // padded with empty space all around
     let mut exploded_map = vec![vec![false; 2 * width - 1 + 2]; 2 * height - 1 + 2];
-    let mut inside = HashSet::new();
 
-    for &(x, y) in distance_map.keys() {
-        let pipe = map[y][x].unwrap();
-
-        let x = 2 * (x - left) + 1;
-        let y = 2 * (y - top) + 1;
-        exploded_map[y][x] = true;
-
-        let neighbors = match pipe {
-            Pipe::NS => [(x, y - 1), (x, y + 1)],
-            Pipe::EW => [(x + 1, y), (x - 1, y)],
-            Pipe::NE => [(x + 1, y), (x, y - 1)],
-            Pipe::NW => [(x, y - 1), (x - 1, y)],
-            Pipe::SW => [(x - 1, y), (x, y + 1)],
-            Pipe::SE => [(x, y + 1), (x + 1, y)],
-        };
-        for (nx, ny) in neighbors {
-            if let Some(cell) = exploded_map.get_mut(ny).and_then(|row| row.get_mut(nx)) {
-                *cell = true;
-            }
-        }
-    }
-
-    // print out exploded map
-    // x' = 2 * (x - left) + 1 => x = (x' - 1) / 2 + left
-
-    let mut part2 = 0;
-    for (x, y) in candidates {
-        // if (x, y) != (5, 3){
-        //     continue;
-        // }
-
-        let mut q = vec![(2 * (x - left) + 1, 2 * (y - top) + 1)];
-        let mut visited = HashSet::new();
-        let mut broke = false;
-
-        while let Some((x, y)) = q.pop() {
-            // println!("{x},{y}");
-            if x == 0
-                || y == 0
-                || !(left..=right).contains(&((x - 1) / 2 + left))
-                || !(top..=bottom).contains(&((y - 1) / 2 + top))
-            {
-                // dbg!();
-                broke = true;
-                break;
-            }
-            if exploded_map.get(y).and_then(|row| row.get(x)).copied().unwrap_or(false) {
-                // dbg!();
-                continue;
-            }
-            if !visited.insert((x, y)) {
-                // dbg!();
-                continue;
-            }
-
-            let neighbors = [
-                (Some(x), y.checked_sub(1)),
-                (Some(x), Some(y + 1)),
-                (Some(x + 1), Some(y)),
-                (x.checked_sub(1), Some(y)),
-                (Some(x + 1), y.checked_sub(1)),
-                (x.checked_sub(1), y.checked_sub(1)),
-                (Some(x + 1), Some(y + 1)),
-                (x.checked_sub(1), Some(y + 1)),
-            ];
-            q.extend(neighbors.iter().filter_map(|&(x, y)| Some((x?, y?))));
-        }
-
-        // dbg!(visited.len(), broke);
-
-        if broke {
-            // for (yp, row) in exploded_map.iter().enumerate() {
-            //     for (xp, cell) in row.iter().enumerate() {
-            //         if (xp,yp) == (2*(x-left),2*(y-top))  {
-            //             print!("\x1b[1;32m{}\x1b[0m", 'K');
-            //         }
-            //         else if visited.contains(&(xp, yp)) {
-            //             print!("\x1b[1;31m{}\x1b[0m", '*');
-            //         }
-            //         else if xp % 2 == 0 && yp % 2 == 0 {
-            //             let x = xp / 2 + left;
-            //             let y = yp / 2 + top;
-            //             if let Some(pipe) = map[y][x] {
-            //                 print!("\x1b[1m{}\x1b[0m", pipe);
-            //             } else {
-            //                 print!(".");
-            //             }
-            //         } else {
-            //             print!("{}", if *cell { '\u{2588}' } else { '.' });
-            //         }
-            //     }
-            //     println!();
-            // }
-        } else {
-            part2 += 1;
-            // TODO: inside.extend(visited)?
-            inside.insert((x, y));
-        }
-
-        // break;
-    }
-
-    // print exploded map
-    println!("EXPLODED:");
-    for (yp, row) in exploded_map.iter().enumerate() {
-        for (xp, cell) in row.iter().enumerate() {
-            if xp % 2 == 1 && yp % 2 == 1 {
-                let x = (xp - 1) / 2 + left;
-                let y = (yp - 1) / 2 + top;
-                if inside.contains(&(x, y)) {
-                    print!("\x1b[1;31m{}\x1b[0m", 'I');
-                } else if let Some(pipe) = map[y][x] {
-                    print!(
-                        "\x1b[{}m{}\x1b[0m",
-                        if distance_map.contains_key(&(x, y)) { "35" } else { "2" },
-                        pipe
-                    );
-                } else {
-                    print!("\x1b[32mO\x1b[0m");
-                }
-            } else {
-                print!("{}", if *cell { '\u{2588}' } else { '.' });
-            }
-        }
-        println!();
-    }
-
-    // print non exploded
-    println!();
-    println!("NON-EXPLODED:");
     for y in top..=bottom {
         for x in left..=right {
-            if inside.contains(&(x, y)) {
-                print!("\x1b[1;31m{}\x1b[0m", 'I');
-            } else if let Some(pipe) = map[y][x] {
-                print!("\x1b[1m{}\x1b[0m", pipe);
-            } else {
-                print!("\x1b[32mO\x1b[0m");
+            if distance_map[y][x] != usize::MAX {
+                let pipe = map[y][x].unwrap();
+
+                let x = 2 * (x - left) + 1;
+                let y = 2 * (y - top) + 1;
+                exploded_map[y][x] = true;
+
+                let neighbors = match pipe {
+                    Pipe::NS => [(x, y - 1), (x, y + 1)],
+                    Pipe::EW => [(x + 1, y), (x - 1, y)],
+                    Pipe::NE => [(x + 1, y), (x, y - 1)],
+                    Pipe::NW => [(x, y - 1), (x - 1, y)],
+                    Pipe::SW => [(x - 1, y), (x, y + 1)],
+                    Pipe::SE => [(x, y + 1), (x + 1, y)],
+                };
+                for (nx, ny) in neighbors {
+                    if let Some(cell) = exploded_map.get_mut(ny).and_then(|row| row.get_mut(nx)) {
+                        *cell = true;
+                    }
+                }
             }
         }
-        println!();
     }
+
+    // x' = 2 * (x - left) + 1 => x = (x' - 1) / 2 + left
+    let part2 = candidates
+        .collect::<Vec<_>>()
+        .into_par_iter()
+        .filter(|(x, y)| {
+            let mut q = vec![(2 * (x - left) + 1, 2 * (y - top) + 1)];
+
+            let mut visited = HashSet::new();
+
+            while let Some((x, y)) = q.pop() {
+                if x == 0
+                    || y == 0
+                    || !(left..=right).contains(&((x - 1) / 2 + left))
+                    || !(top..=bottom).contains(&((y - 1) / 2 + top))
+                {
+                    return false;
+                }
+                if exploded_map.get(y).and_then(|row| row.get(x)).copied().unwrap_or(false) || !visited.insert((x, y)) {
+                    continue;
+                }
+
+                let neighbors = [
+                    (Some(x), y.checked_sub(1)),
+                    (Some(x), Some(y + 1)),
+                    (Some(x + 1), Some(y)),
+                    (x.checked_sub(1), Some(y)),
+                    (Some(x + 1), y.checked_sub(1)),
+                    (x.checked_sub(1), y.checked_sub(1)),
+                    (Some(x + 1), Some(y + 1)),
+                    (x.checked_sub(1), Some(y + 1)),
+                ];
+                q.extend(neighbors.iter().filter_map(|&(x, y)| Some((x?, y?))));
+            }
+
+            true
+        })
+        .count();
 
     (part1, part2)
 }
