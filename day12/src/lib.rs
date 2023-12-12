329074f7ff672dc0b8ad0e540b509710388c387d
PurpleMyst <PurpleMyst@users.noreply.github.com>
2023-12-12 22:23:18 +0100

    perf(day12): adapt redditor solution :>

--- day12/src/lib.rs
+++ day12/src/lib.rs
@@ -1,148 +1,120 @@
-use std::{collections::HashMap, fmt::Display};
+use std::fmt::Display;
 
+use ahash::{HashMap, HashMapExt};
 use rayon::prelude::*;
 
-const WORKING: u8 = b'.';
-const BROKEN: u8 = b'#';
-const UNKNOWN: u8 = b'?';
+// Adapted from https://redd.it/18gomx5 with the help of ChatGPT and GitHub Copilot!
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-struct State {
-    records: &'static [u8],
-    current_group: Option<u8>,
-    remaining_groups: &'static [u8],
+fn part1(input: &str) -> usize {
+    input
+        .lines()
+        .collect::<Vec<_>>()
+        .into_par_iter()
+        .map(|line| create_permutations(line))
+        .sum()
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-enum RecordResult {
-    Die,
-    Continue(State),
-    Branch(State, State),
+fn part2(input: &str) -> usize {
+    input
+        .lines()
+        .collect::<Vec<_>>()
+        .into_par_iter()
+        .map(unfold)
+        .map(|line| create_permutations(&line))
+        .sum()
 }
 
-fn process_record(current_record: u8, remaining_records: &'static [u8], state: State) -> RecordResult {
-    match current_record {
-        WORKING => {
-            // We've found a working one!
-            match state.current_group {
-                // If there were no more broken springs remaining in our current group,
-                // this is what we should expect: the state should continue to live.
-                // This is also what should happen if we weren't even in a group
-                Some(0) | None => RecordResult::Continue(State {
-                    records: remaining_records,
-                    current_group: None,
-                    remaining_groups: state.remaining_groups,
-                }),
-
-                // If we were expecting more broken springs and we found a working one, this state
-                // should die as it is based on an invalid hypothesis.
-                Some(..) => RecordResult::Die,
-            }
+fn unfold(line: &str) -> String {
+    let (springs, config) = line.split_once(' ').unwrap();
+    let mut unfolded_springs = String::new();
+    let mut unfolded_config = String::new();
+    for i in 0..5 {
+        if i != 0 {
+            unfolded_springs.push('?');
+            unfolded_config.push(',');
         }
+        unfolded_springs.push_str(springs);
+        unfolded_config.push_str(config);
+    }
+    format!("{} {}", unfolded_springs, unfolded_config)
+}
 
-        BROKEN => {
-            // We've found a broken one!
-            match state.current_group {
-                // If we were expecting no more broken ones, this state should die as it is
-                // based on an invalid hypothesis.
-                Some(0) => RecordResult::Die,
+fn create_permutations(line: &str) -> usize {
+    let (all_springs, spring_condition) = line.split_once(' ').unwrap();
 
-                // If we weren't in a group, let's start one.
-                None => {
-                    if let Some((&current_group, remaining_groups)) = state.remaining_groups.split_first() {
-                        RecordResult::Continue(State {
-                            records: remaining_records,
-                            current_group: Some(current_group - 1),
-                            remaining_groups,
-                        })
-                    } else {
-                        RecordResult::Die
+    let conditions: Vec<usize> = spring_condition.split(',').map(|s| s.parse().unwrap()).collect();
+    let springs: Vec<char> = all_springs.chars().collect();
+
+    let mut spring_permutations = vec![(0, 0, 1)];
+    let mut spring_permutation_counts: HashMap<(usize, usize), usize> = HashMap::new();
+    spring_permutation_counts.insert((0, 0), 1);
+
+    let mut springs_checked = 0;
+    for &spring in springs.iter() {
+        let mut new_permutations = vec![];
+        for (&(group_idx, group_len), &permutations) in spring_permutation_counts.iter() {
+            match spring {
+                '#' => {
+                    // If we haven't run out of groups and the current group is not full, we can
+                    // advance this state.
+                    if group_idx < conditions.len() && group_len < conditions[group_idx] {
+                        new_permutations.push((group_idx, group_len + 1, permutations));
                     }
                 }
+                '?' => {
+                    // Process as #
+                    if group_idx < conditions.len() && group_len < conditions[group_idx] {
+                        new_permutations.push((group_idx, group_len + 1, permutations));
+                    }
 
-                // At this point, we're in a group where we were expecting at least one more
-                // broken spring. Let's update the state!
-                Some(n) => RecordResult::Continue(State {
-                    records: remaining_records,
-                    current_group: Some(n - 1),
-                    remaining_groups: state.remaining_groups,
-                }),
-            }
-        }
-
-        UNKNOWN => {
-            match (
-                process_record(WORKING, remaining_records, state.clone()),
-                process_record(BROKEN, remaining_records, state),
-            ) {
-                (RecordResult::Die, RecordResult::Die) => RecordResult::Die,
-                (RecordResult::Die, r @ RecordResult::Continue(..)) => r,
-                (r @ RecordResult::Continue(..), RecordResult::Die) => r,
-                (RecordResult::Continue(s1), RecordResult::Continue(s2)) => RecordResult::Branch(s1, s2),
+                    // Process as .
+                    if group_len == 0 {
+                        new_permutations.push((group_idx, group_len, permutations));
+                    } else if group_len == conditions[group_idx] {
+                        new_permutations.push((group_idx + 1, 0, permutations));
+                    }
+                }
+                '.' => {
+                    if group_len == 0 {
+                        // We're not in a group, so leave the state as-is.
+                        new_permutations.push((group_idx, group_len, permutations));
+                    } else if group_len == conditions[group_idx] {
+                        // The current group has been broken. Advance to the next group.
+                        new_permutations.push((group_idx + 1, 0, permutations));
+                    }
+                }
                 _ => unreachable!(),
             }
         }
 
-        _ => unreachable!(),
-    }
-}
+        spring_permutations = new_permutations;
+        springs_checked += 1;
 
-#[michie::memoized(key_expr = state, store_type = HashMap<State, u64>)]
-fn process_state(state: State) -> u64 {
-    let Some((&current_record, remaining_records)) = state.records.split_first() else {
-        let is_valid = matches!(state.current_group, Some(0) | None) && state.remaining_groups.is_empty();
-        return if is_valid { 1 } else { 0 };
-    };
-    match process_record(current_record, remaining_records, state) {
-        RecordResult::Die => 0,
-        RecordResult::Continue(s) => process_state(s),
-        RecordResult::Branch(s1, s2) => process_state(s1) + process_state(s2),
-    }
-}
+        let springs_left = all_springs.len() - springs_checked;
+        spring_permutations = spring_permutations
+            .into_iter()
+            .filter(|&(group_idx, group_len, _)| {
+                if group_idx > conditions.len() {
+                    group_len == 0
+                } else {
+                    springs_left + group_len >= conditions.iter().skip(group_idx).sum()
+                }
+            })
+            .collect();
 
-fn process_row(records: &'static [u8], groups: &'static [u8]) -> u64 {
-    return process_state(State {
-        records,
-        current_group: None,
-        remaining_groups: groups,
-    });
+        spring_permutation_counts.clear();
+        for &(group, amount, permutations) in &spring_permutations {
+            *spring_permutation_counts.entry((group, amount)).or_insert(0) += permutations;
+        }
+    }
+
+    spring_permutations
+        .into_iter()
+        .map(|(_, _, permutations)| permutations)
+        .sum()
 }
 
 #[inline]
 pub fn solve() -> (impl Display, impl Display) {
-    let input = include_str!("input.txt").lines().map(|line| {
-        let (records, groups) = line.split_once(' ').unwrap();
-        let records = records.as_bytes();
-        let groups = groups
-            .split(',')
-            .map(|group| group.parse::<u8>().unwrap())
-            .collect::<Vec<_>>();
-        (records, groups)
-    });
-
-    let part1 = input
-        .clone()
-        .map(|(records, groups)| process_row(records, groups.leak()))
-        .sum::<u64>();
-
-    let part2 = input
-        .collect::<Vec<_>>()
-        .into_par_iter()
-        .map(|(records, groups)| {
-            let mut new_records = Vec::new();
-            for i in 0..5 {
-                if i != 0 {
-                    new_records.push(UNKNOWN);
-                }
-                new_records.extend_from_slice(records);
-            }
-            let mut new_groups = Vec::new();
-            for _ in 0..5 {
-                new_groups.extend_from_slice(&groups);
-            }
-            process_row(new_records.leak(), new_groups.leak())
-        })
-        .sum::<u64>();
-
-    (part1, part2)
+    rayon::join(|| part1(include_str!("input.txt")), || part2(include_str!("input.txt")))
 }
