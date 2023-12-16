f940f519c2d3d9f5a6895f09804c6084237de7ee
PurpleMyst <PurpleMyst@users.noreply.github.com>
2023-12-16 15:07:37 +0100

    perf(day16): use Vec to store beams
    deduplication is already handled via the bitset, hashing just adds
    slowness.

--- day16/src/lib.rs
+++ day16/src/lib.rs
@@ -2,7 +2,6 @@ use std::{cmp::max, fmt::Display, mem::swap};
 
 use grid::Grid;
 use rayon::prelude::*;
-use rustc_hash::FxHashSet as HashSet;
 
 type Coordinate = u8;
 type Position = (Coordinate, Coordinate);
@@ -19,7 +18,7 @@ enum Cell {
 use Cell::*;
 
 impl Cell {
-    fn apply_to(self, pos: Position, coming_from: Direction, beams: &mut HashSet<(Position, Direction)>) {
+    fn apply_to(self, pos: Position, coming_from: Direction, beams: &mut Vec<(Position, Direction)>) {
         match (self, coming_from) {
             // Empty tiles or pointy ends just pass the beam through.
             (Empty, _)
@@ -27,35 +26,35 @@ impl Cell {
             | (VerticalSplitter, Down)
             | (HorizontalSplitter, Left)
             | (HorizontalSplitter, Right) => {
-                beams.insert((pos, coming_from));
+                beams.push((pos, coming_from));
             }
 
             // The mirrors reflect, obviously.
             (ForwardMirror, Right) | (BackMirror, Left) => {
-                beams.insert((pos, Up));
+                beams.push((pos, Up));
             }
 
             (ForwardMirror, Up) | (BackMirror, Down) => {
-                beams.insert((pos, Right));
+                beams.push((pos, Right));
             }
 
             (ForwardMirror, Left) | (BackMirror, Right) => {
-                beams.insert((pos, Down));
+                beams.push((pos, Down));
             }
 
             (ForwardMirror, Down) | (BackMirror, Up) => {
-                beams.insert((pos, Left));
+                beams.push((pos, Left));
             }
 
             // The splitters... split!
             (VerticalSplitter, Left) | (VerticalSplitter, Right) => {
-                beams.insert((pos, Up));
-                beams.insert((pos, Down));
+                beams.push((pos, Up));
+                beams.push((pos, Down));
             }
 
             (HorizontalSplitter, Up) | (HorizontalSplitter, Down) => {
-                beams.insert((pos, Left));
-                beams.insert((pos, Right));
+                beams.push((pos, Left));
+                beams.push((pos, Right));
             }
         }
     }
@@ -82,16 +81,16 @@ impl Direction {
     }
 }
 
-fn do_solve(map: &Map, insertion_point: Position, initial_dir: Direction) -> usize {
-    let mut beams = HashSet::default();
-    let mut new_beams = HashSet::default();
+fn do_solve(map: &Map, pushion_point: Position, initial_dir: Direction) -> usize {
+    let mut beams = Vec::default();
+    let mut new_beams = Vec::default();
     let mut seen_beams = Grid::<u8>::new(map.rows(), map.cols());
 
-    // Account for the possibility that the insertion point is already some sort of special tile.
-    map[(insertion_point.1 as usize, insertion_point.0 as usize)].apply_to(insertion_point, initial_dir, &mut beams);
+    // Account for the possibility that the pushion point is already some sort of special tile.
+    map[(pushion_point.1 as usize, pushion_point.0 as usize)].apply_to(pushion_point, initial_dir, &mut beams);
 
     while !beams.is_empty() {
-        for ((x, y), dir) in beams.drain() {
+        for ((x, y), dir) in beams.drain(..) {
             // If there's already been a beam in this spot and moving in this direction, there's no need to recalculate.
             let pos_seen = &mut seen_beams[(y as usize, x as usize)];
             if (*pos_seen) & (1 << dir as u8) != 0 {
