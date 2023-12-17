0f90507c0adf085c06bd3cb480bb48df7704eb3d
PurpleMyst <PurpleMyst@users.noreply.github.com>
2023-12-17 15:17:57 +0100

    perf(day17): specialized implementation adapted from reddit

--- day17/src/lib.rs
+++ day17/src/lib.rs
@@ -1,112 +1,114 @@
 use std::fmt::Display;
 
-use pathfinding::prelude::*;
+// Adaptation of https://www.reddit.com/r/adventofcode/comments/18k9ne5/2023_day_17_solutions/kdqeywd/
+// With the addition of bucket queue + my own optimizations
 
-// Forward in the directions array = rotate 90° counter-clockwise;
-// Backward in the directions array = rotate 90° clockwise;
-const DIRECTIONS: [(isize, isize); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+use ::bucket_queue::*;
 
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
-struct State {
-    position: (usize, usize),
-    direction: usize,
-    steps_before_can_turn: usize,
-    steps_before_must_turn: usize,
+const UP: u8 = 0;
+const RIGHT: u8 = 1;
+const DOWN: u8 = 2;
+const LEFT: u8 = 3;
+
+struct Grid {
+    costs: Box<[u8]>,
+    columns: usize,
 }
 
-fn try_add_dir(pos: (usize, usize), dir: (isize, isize)) -> Option<(usize, usize)> {
-    Some((pos.0.checked_add_signed(dir.0)?, pos.1.checked_add_signed(dir.1)?))
+impl Grid {
+    fn new(s: &str) -> Self {
+        let mut lines = s.lines().peekable();
+        let columns = lines.peek().map_or(0, |l| l.len());
+        Self {
+            costs: lines.flat_map(str::as_bytes).map(|&c| c - b'0').collect::<Box<_>>(),
+            columns,
+        }
+    }
+
+    fn step(&self, pos: usize, dir: u8) -> Option<usize> {
+        let rows = self.costs.len();
+        Some(match dir {
+            UP if pos > self.columns => pos - self.columns,
+            RIGHT if (pos + 1) % self.columns != 0 => pos + 1,
+            DOWN if pos < rows - self.columns => pos + self.columns,
+            LEFT if pos % self.columns != 0 => pos - 1,
+            _ => return None,
+        })
+    }
 }
 
-fn do_solve(
-    map: &Matrix<u64>,
-    min_straight_steps: usize,
-    max_straight_steps: usize,
-    starting_dir: usize,
-) -> Option<u64> {
-    let goal = (map.columns() - 1, map.rows() - 1);
+fn do_solve(input: &str, min_straight_steps: usize, max_straight_steps: usize) -> Option<usize> {
+    let grid = Grid::new(input);
+    let start = 0;
+    let goal = grid.costs.len() - 1;
 
-    let (_states, answer) = dijkstra(
-        &State {
-            position: (0, 0),
-            direction: starting_dir,
-            steps_before_must_turn: max_straight_steps - 1,
-            steps_before_can_turn: min_straight_steps.saturating_sub(1),
-        },
-        |state| {
-            let mut next = Vec::new();
+    let mut visited = vec![0u8; grid.costs.len()];
+    let mut cost_cache = vec![usize::MAX; 2 * grid.costs.len()];
+    let mut q: BucketQueue<Vec<_>> = BucketQueue::new();
 
-            let current_dir = DIRECTIONS[state.direction];
+    q.push((start, UP), 0);
+    q.push((start, RIGHT), 0);
 
-            if state.steps_before_can_turn == 0 {
-                let left_dir_idx = (state.direction + 1) % DIRECTIONS.len();
-                let left_dir = DIRECTIONS[left_dir_idx];
-                if let Some(left_pos) = try_add_dir(state.position, left_dir) {
-                    if let Some(&cost) = map.get((left_pos.1, left_pos.0)) {
-                        next.push((
-                            State {
-                                position: left_pos,
-                                direction: left_dir_idx,
-                                steps_before_must_turn: max_straight_steps - 1,
-                                steps_before_can_turn: min_straight_steps.saturating_sub(1),
-                            },
-                            cost,
-                        ));
-                    }
-                }
+    // Here's the idea: There's no meaningful difference between horizontal and vertical movement. At each step, we
+    // can just go straight any allowed number of straight steps, then turn.
 
-                let right_dir_idx = (state.direction + DIRECTIONS.len() - 1) % DIRECTIONS.len();
-                let right_dir = DIRECTIONS[right_dir_idx];
-                if let Some(right_pos) = try_add_dir(state.position, right_dir) {
-                    if let Some(&cost) = map.get((right_pos.1, right_pos.0)) {
-                        next.push((
-                            State {
-                                position: right_pos,
-                                direction: right_dir_idx,
-                                steps_before_must_turn: max_straight_steps - 1,
-                                steps_before_can_turn: min_straight_steps.saturating_sub(1),
-                            },
-                            cost,
-                        ));
-                    }
-                }
+    while let Some(cost) = q.min_priority() {
+        // Our state is: the current position and what direction we were facing when we got here.
+        let (pos, previous_dir) = q.pop(cost).unwrap();
+
+        // If this is the goal, great!
+        if pos == goal {
+            return Some(cost);
+        }
+
+        // If we've already visited this position from this direction, we can skip it.
+        if visited[pos] & (1u8 << previous_dir) != 0 {
+            continue;
+        }
+        visited[pos] |= 1u8 << previous_dir;
+
+        let left_dir = previous_dir ^ 1;
+        let right_dir = left_dir ^ 2;
+        // For each possible turning direction...
+        'turnloop: for turn_dir in [left_dir, right_dir] {
+            let mut new_cost = cost;
+            let mut new_pos = pos;
+
+            // Go straight in the new direction the requisite amount of steps.
+            for _ in 1..min_straight_steps {
+                new_pos = if let Some(np) = grid.step(new_pos, turn_dir) {
+                    np
+                } else {
+                    continue 'turnloop;
+                };
+                new_cost += grid.costs[new_pos] as usize;
             }
 
-            if let Some(straight_pos) = try_add_dir(state.position, current_dir) {
-                if let Some(&cost) = map.get((straight_pos.1, straight_pos.0)) {
-                    if let Some(steps_left) = state.steps_before_must_turn.checked_sub(1) {
-                        next.push((
-                            State {
-                                position: straight_pos,
-                                direction: state.direction,
-                                steps_before_must_turn: steps_left,
-                                steps_before_can_turn: state.steps_before_can_turn.saturating_sub(1),
-                            },
-                            cost,
-                        ));
-                    }
+            // After we've exhausted the minimum number of straight steps, we can choose to stop walking and allow
+            // another opportunity to turn.
+            for _ in min_straight_steps..=max_straight_steps {
+                new_pos = if let Some(np) = grid.step(new_pos, turn_dir) {
+                    np
+                } else {
+                    break;
+                };
+                new_cost += grid.costs[new_pos] as usize;
+
+                // Stop walking! We only add the state to the queue if it's better than what we've seen before.
+                let cache_idx = (new_pos << 1) | left_dir as usize;
+                if cost_cache[cache_idx] > new_cost {
+                    cost_cache[cache_idx] = new_cost;
+                    q.push((new_pos, left_dir), new_cost);
                 }
             }
+        }
+    }
 
-            next
-        },
-        |state| state.position == goal && state.steps_before_can_turn == 0,
-    )?;
-
-    Some(answer)
+    None
 }
 
 #[inline]
 pub fn solve() -> (impl Display, impl Display) {
-    let map = include_str!("input.txt")
-        .lines()
-        .map(|line| line.bytes().map(|b| b - b'0').map(u64::from).collect::<Vec<_>>())
-        .collect::<Matrix<_>>();
-
-    let p1 = do_solve(&map, 0, 3, 1).unwrap();
-    let p2 = (0..DIRECTIONS.len())
-        .filter_map(|dir| do_solve(&map, 4, 10, dir))
-        .min()
-        .unwrap();
-    (p1, p2)
+    let input = include_str!("input.txt");
+    rayon::join(|| do_solve(input, 0, 3).unwrap(), || do_solve(input, 4, 10).unwrap())
 }
