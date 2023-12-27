74034dc6c2d8183a0b4241bf16a06797c67ec1a1
PurpleMyst <PurpleMyst@users.noreply.github.com>
2023-12-27 13:56:20 +0100

    perf(day23): implement State as a sorted ArrayVec

--- day23/src/part2.rs
+++ day23/src/part2.rs
@@ -1,14 +1,57 @@
-use std::{collections::VecDeque, hash::BuildHasherDefault};
-use std::mem::swap;
+use std::{collections::VecDeque, hash::BuildHasherDefault, mem::swap};
 
 use arrayvec::ArrayVec;
 use rustc_hash::{FxHashMap as HashMap, FxHasher};
 
-type HashSet<T> = im::HashSet<T, BuildHasherDefault<FxHasher>>;
+type OriginalNode = usize;
+type Node = u8;
 
-type Node = usize;
 type Cost = u16;
-type States = HashMap<HashSet<(Node, Node)>, Cost>;
+type States = HashMap<State, Cost>;
+
+#[derive(Clone, Debug, Default, Eq, Hash, PartialEq)]
+struct State {
+    paths: ArrayVec<(Node, Node), 10>,
+}
+
+impl State {
+    fn insert(&mut self, edge: (Node, Node)) {
+        let insertion_idx = self.paths.binary_search(&edge).unwrap_or_else(|e| e);
+        self.paths.insert(insertion_idx, edge);
+    }
+
+    fn remove(&mut self, edge: (Node, Node)) {
+        if let Ok(idx) = self.paths.binary_search(&edge) {
+            self.paths.remove(idx);
+        } else {
+            panic!("edge not found");
+        }
+    }
+
+    fn find_edge_containing(&self, node: Node) -> Option<(Node, Node)> {
+        self.paths.iter().find(|&&(u, v)| u == node || v == node).copied()
+    }
+
+    fn find_distinct_edges_containing(&self, node1: Node, node2: Node) -> Option<((Node, Node), (Node, Node))> {
+        let mut edge1 = None;
+        let mut edge2 = None;
+        for &edge in self.paths.iter() {
+            if edge.0 == node1 || edge.1 == node1 {
+                edge1 = Some(edge);
+                if edge2.is_some() {
+                    break;
+                }
+            }
+            if edge.0 == node2 || edge.1 == node2 {
+                edge2 = Some(edge);
+                if edge1.is_some() {
+                    break;
+                }
+            }
+        }
+        edge1.zip(edge2).filter(|&(e1, e2)| e1 != e2)
+    }
+}
 
 struct Solver {
     states: States,
@@ -20,7 +63,7 @@ struct Solver {
 impl Solver {
     fn new(start: Node, goal: Node) -> Self {
         let mut states: States = Default::default();
-        states.insert(HashSet::default(), 0);
+        states.insert(Default::default(), 0);
 
         Self {
             states,
@@ -46,9 +89,9 @@ impl Solver {
         }
 
         for (mut state, cost) in self.states.drain() {
-            if let Some(&edge) = state.iter().find(|&&(a, b)| a == vertex || b == vertex) {
+            if let Some(edge) = state.find_edge_containing(vertex) {
                 if edge == (vertex, vertex) {
-                    state.remove(&edge);
+                    state.remove(edge);
                     self.new_states
                         .entry(state)
                         .and_modify(|c| *c = (*c).max(cost))
@@ -69,31 +112,15 @@ impl Solver {
         debug_assert_eq!(self.new_states.len(), 0);
         let old_dp = self.states.clone();
         for (mut state, cost) in old_dp {
-            let mut u_edge = None;
-            let mut v_edge = None;
-            for &edge in state.iter() {
-                if edge.0 == u || edge.1 == u {
-                    u_edge = Some(edge);
-                }
-                if edge.0 == v || edge.1 == v {
-                    v_edge = Some(edge);
-                }
-            }
-            let Some(u_edge) = u_edge else {
+            let Some((u_edge, v_edge)) = state.find_distinct_edges_containing(u, v) else {
                 continue;
             };
-            let Some(v_edge) = v_edge else {
-                continue;
-            };
-            if u_edge == v_edge {
-                continue;
-            }
             let x = if u_edge.0 == u { u_edge.1 } else { u_edge.0 };
             let y = if v_edge.0 == v { v_edge.1 } else { v_edge.0 };
             let new_edge = if x < y { (x, y) } else { (y, x) };
             let new_cost = cost + w;
-            state.remove(&u_edge);
-            state.remove(&v_edge);
+            state.remove(u_edge);
+            state.remove(v_edge);
             state.insert(new_edge);
             self.states
                 .entry(state)
@@ -148,15 +175,15 @@ pub fn solve(input: &str) -> Cost {
         })
         .collect::<Vec<_>>();
 
-    let mut graph = vec![ArrayVec::<(Node, Cost), 4>::new(); area];
+    let mut graph = vec![ArrayVec::<(OriginalNode, Cost), 4>::new(); area];
 
     let mut outer_visited = vec![false; area];
-    let mut outer_queue: VecDeque<Node> = Default::default();
+    let mut outer_queue: VecDeque<OriginalNode> = Default::default();
     outer_queue.push_back(start_idx);
 
     while let Some(source) = outer_queue.pop_front() {
         let mut visited = vec![false; area];
-        let mut queue: VecDeque<(Node, Cost)> = Default::default();
+        let mut queue: VecDeque<(OriginalNode, Cost)> = Default::default();
         queue.push_back((source, 0));
 
         while let Some((node, cost)) = queue.pop_front() {
@@ -179,40 +206,68 @@ pub fn solve(input: &str) -> Cost {
         }
     }
 
+    // reduce graph indices to u8
+    let mut i: Node = 0;
+    let mut new_start_idx = 0;
+    let mut new_goal_idx = 0;
+    let mut new_indices = vec![0; area];
+    for (idx, neighbors) in graph.iter_mut().enumerate() {
+        if neighbors.len() > 0 {
+            if usize::from(idx) == start_idx {
+                new_start_idx = i;
+            }
+            if usize::from(idx) == goal_idx {
+                new_goal_idx = i;
+            }
+            new_indices[idx] = i;
+            i += 1;
+        }
+    }
+    let mut new_graph = vec![ArrayVec::<(Node, Cost), 4>::new(); usize::from(i)];
+    for (idx, neighbors) in graph.iter_mut().enumerate() {
+        if neighbors.len() > 0 {
+            for &(neighbor, weight) in neighbors.iter() {
+                new_graph[usize::from(new_indices[idx])].push((new_indices[usize::from(neighbor)], weight));
+            }
+        }
+    }
+    let graph = new_graph;
+    let start_idx = new_start_idx;
+    let goal_idx = new_goal_idx;
+
     let mut solver = Solver::new(start_idx, goal_idx);
 
     let mut q = VecDeque::new();
     let mut visited = vec![false; area];
     let mut known = vec![false; area];
     q.push_back(start_idx);
-    visited[start_idx] = true;
-
+    visited[usize::from(start_idx)] = true;
 
     let mut unexplored = graph.iter().map(|neighbors| neighbors.len()).collect::<Vec<_>>();
 
     while let Some(node) = q.pop_front() {
         solver.add_vertex(node);
-        known[node] = true;
+        known[usize::from(node)] = true;
 
-        for &(neighbor, weight) in &graph[node] {
-            if known[neighbor] {
+        for &(neighbor, weight) in &graph[usize::from(node)] {
+            if known[usize::from(neighbor)] {
                 solver.add_edge(node, neighbor, weight);
-                unexplored[node] -= 1;
-                unexplored[neighbor] -= 1;
-                if unexplored[node] == 0 {
+                unexplored[usize::from(node)] -= 1;
+                unexplored[usize::from(neighbor)] -= 1;
+                if unexplored[usize::from(node)] == 0 {
                     solver.remove_vertex(node);
                 }
-                if unexplored[neighbor] == 0 {
+                if unexplored[usize::from(neighbor)] == 0 {
                     solver.remove_vertex(neighbor);
                 }
-            } else if !visited[neighbor] {
-                visited[neighbor] = true;
+            } else if !visited[usize::from(neighbor)] {
+                visited[usize::from(neighbor)] = true;
                 q.push_back(neighbor);
             }
         }
     }
 
-    let mut sol_set = HashSet::default();
+    let mut sol_set = State::default();
     sol_set.insert((start_idx, goal_idx));
     solver.states[&sol_set]
 }
