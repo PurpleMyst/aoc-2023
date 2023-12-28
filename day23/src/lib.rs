03dd2b846de555b9d824e23d9ef9c49aab9e6cb7
PurpleMyst <PurpleMyst@users.noreply.github.com>
2023-12-28 15:46:38 +0100

    refactor(day23): avoid replacing as that's now handled in parsing code

--- day23/src/lib.rs
+++ day23/src/lib.rs
@@ -6,11 +6,5 @@ pub mod part2;
 #[inline]
 pub fn solve() -> (impl Display, impl Display) {
     let input = include_str!("input.txt");
-
-    let part1 = part1::solve(input);
-
-    let part2_input = input.replace(['>', '<', '^', 'v'], ".");
-    let part2 = part2::solve(&part2_input);
-
-    (part1, part2)
+    (part1::solve(input), part2::solve(input))
 }
